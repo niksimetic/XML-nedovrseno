4f890d08b2e5401d49b82c2f13d2a32607c58e39
niksimetic <55134764+niksimetic@users.noreply.github.com>
2021-05-04 10:55:27 +0200

    Add files via upload

---
+++ nikSimeticXMLforma/nikSimeticXMLforma/Pregled Podataka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nikSimeticXMLforma
+{
+    public partial class Pregled_Podataka : Form
+    {
+        public Pregled_Podataka()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
